83315622de6d1f13df8a6c757d0f5ee36972a4f4
karanshetty121-dev <karanshetty121@gmail.com>
2026-01-08 19:58:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ if check_password():
     show_brand_table("Bridgestone Master Data", bridgestone_master)
     show_brand_table("Firestone Master Data", firestone_master)
     show_brand_table("Apollo Master Data", apollo_master)
-    show_brand_table("JK Tyre Data", jk_master)
+    show_brand_table("JK Tyre Master Data", jk_master)
